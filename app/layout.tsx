05cadf66038c77e69a4cc69646f8de983305a979
pan93412 <pan93412@gmail.com>
2022-12-20 23:06:19 +0800

    refactor!: Remove unnecessary fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import "@styles/globals.scss";
 // eslint-disable-next-line camelcase
-import { Cabin, IBM_Plex_Mono } from "@next/font/google";
-import Local from "@next/font/local";
+import { Cabin } from "@next/font/google";
 import cx from "classix";
 import { AnalyticsWrapper } from "./analytics";
 
@@ -9,45 +8,6 @@ import { AnalyticsWrapper } from "./analytics";
 const cabin = Cabin({
   variable: "--font-cabin",
   subsets: ["latin"],
-  preload: true,
-});
-
-// eslint-disable-next-line new-cap
-const ibmPlexMono = IBM_Plex_Mono({
-  variable: "--font-ibm-plex-mono",
-  subsets: ["latin"],
-  weight: ["700"],
-});
-
-// eslint-disable-next-line new-cap
-const harmonyosSans = Local({
-  src: [
-    // {
-    //   path: "../vendor/harmonyos-font/HarmonyOS_Sans_TC/HarmonyOS_Sans_TC_Black.woff2",
-    //   weight: "900",
-    // },
-    {
-      path: "../vendor/harmonyos-font/HarmonyOS_Sans_TC/HarmonyOS_Sans_TC_Bold.woff2",
-      weight: "700",
-    },
-    // {
-    //   path: "../vendor/harmonyos-font/HarmonyOS_Sans_TC/HarmonyOS_Sans_TC_Medium.woff2",
-    //   weight: "500",
-    // },
-    {
-      path: "../vendor/harmonyos-font/HarmonyOS_Sans_TC/HarmonyOS_Sans_TC_Regular.woff2",
-      weight: "400",
-    },
-    // {
-    //   path: "../vendor/harmonyos-font/HarmonyOS_Sans_TC/HarmonyOS_Sans_TC_Light.woff2",
-    //   weight: "300",
-    // },
-    // {
-    //   path: "../vendor/harmonyos-font/HarmonyOS_Sans_TC/HarmonyOS_Sans_TC_Thin.woff2",
-    //   weight: "100",
-    // },
-  ],
-  variable: "--font-harmony",
   preload: true,
 });
 
@@ -61,14 +21,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html
-      lang="zh-tw"
-      className={cx(
-        cabin.variable,
-        harmonyosSans.variable,
-        ibmPlexMono.variable
-      )}
-    >
+    <html lang="zh-tw" className={cx(cabin.variable)}>
       <body>
         {children}
         <AnalyticsWrapper />
