79dce7b0acd1d209cfd928369e9b1dd0d681422f
pan93412 <pan93412@gmail.com>
2022-12-26 12:08:22 +0800

    fix(CurrentAge): Don't re-layout

--- app/(main)/(about)/InfoBlock/CurrentAge.tsx
+++ app/(main)/(about)/InfoBlock/CurrentAge.tsx
@@ -9,7 +9,9 @@ const msToYr = (ms: number) => ms * 3.168808781402895 * Math.pow(10, -11);
  * Get the current precise age.
  */
 export function CurrentAge() {
-  const [age, setYears] = useState(2022 - BORN_AT.getFullYear() + "");
+  const [age, setYears] = useState(
+    (2022 - BORN_AT.getFullYear()).toPrecision(12)
+  );
 
   useEffect(() => {
     const preciseAge = msToYr(Number(new Date()) - Number(BORN_AT)).toPrecision(
