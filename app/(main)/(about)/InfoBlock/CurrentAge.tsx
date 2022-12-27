f84aad8b67b80bd9c8fd47299725936c1fcb807d
pan93412 <pan93412@gmail.com>
2022-12-27 11:30:23 +0800

    feat(CurrentAge): Update age automatically

--- app/(main)/(about)/InfoBlock/CurrentAge.tsx
+++ app/(main)/(about)/InfoBlock/CurrentAge.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 const BORN_AT = new Date("2004-04-12T01:00:00+08:00");
 const msToYr = (ms: number) => ms * 3.168808781402895 * Math.pow(10, -11);
@@ -12,13 +12,21 @@ export function CurrentAge() {
   const [age, setYears] = useState(
     (2022 - BORN_AT.getFullYear()).toPrecision(12)
   );
-
-  useEffect(() => {
+  const ageRenderer = useCallback(() => {
     const preciseAge = msToYr(Number(new Date()) - Number(BORN_AT)).toPrecision(
       12
     );
     setYears(preciseAge);
   }, [setYears]);
+
+  useEffect(() => {
+    ageRenderer(); // initial render
+    const interval = setInterval(ageRenderer, 1145.14);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [ageRenderer]);
 
   return <>{age}</>;
 }
